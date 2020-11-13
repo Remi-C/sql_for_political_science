e4253aeed326c3a581ecf414bedfc1290d970605
Remi-C <Remi-C@users.noreply.github.com>
2020-11-13 10:19:02 -0500

    improvement of R demo

--- connect_to_db/connect_to_postgres_R_sample.R
+++ connect_to_db/connect_to_postgres_R_sample.R
@@ -1,12 +1,10 @@
 ## R demo using direct access to postgres table 
 
-​
 library(DBI) # necessary to connect to db
-library(RPostgreSQL) # postgres specific drivers
-#password can only be used locally (after a ssh), so it is safe to share the password.
-​
+library(RPostgreSQL) # postgres specific drivers 
+
 driver <- dbDriver("PostgreSQL")
-​
+
 ##now we connect to the server  
 con <- DBI::dbConnect(drv = driver, 
                       host = "xvii.mit.edu",
@@ -17,28 +15,50 @@ con <- DBI::dbConnect(drv = driver,
 )
 library(dplyr) # data manipulation package
 library(dbplyr) # compatibility with DBI
-​
-#we load a distant table into R. Note that no data is retrieved here.
-#data will be retrieved only when needed.
-#the table is not in the schema "public", but rather in the schema "covid"
-df_distant <- tbl(con, in_schema("covid", "relational___ppp_over_150"))  
-​​
-#actually get the data from the db
-df_distant %>% collect()
-# now do something with the data
 
-​
-# If you want, you can also send raw queries with DBI
-dbListTables(con)
-res <- dbSendQuery(con, "SELECT loan_uid, loan_category, business_name
+
+### First option ###
+# the simplest option to retrieve data would be : 
+
+res2 <- dbGetQuery(con, "SELECT loan_uid, loan_category, business_name
 	, jobs_reported
 	, date_approved 
 FROM covid.relational___ppp_over_150 
 LIMIT 10;")
-dbFetch(res)
-# do something with what you got
+# cool, the result is a nice dataframe ! 
 
-dbClearResult(res)
 
-​# do not forget to close the connection when you are finished
+### Second option ###
+# If you are doing serious stuff and perf matters, 
+# here is a nicer way to get the data and do something with it
+ 
+#we load a distant table into R. Note that no data is retrieved here.
+#data will be retrieved only when needed. 
+df_distant <- tbl(con, in_schema("covid", "relational___ppp_over_150"))  
+
+# Show the data
+df_distant
+
+# run a remote query on the data
+df_distant %>%
+  group_by(loan_range) %>%
+  count() %>%
+  arrange(desc(n))
+
+#actually get the data from the db
+df_local <- df_distant %>% collect()
+
+# now do something with the data
+local_agregate<- df_local %>%
+  filter(owner_gender!="NA", race_ethnicity != "NA")%>%
+  group_by(owner_gender, race_ethnicity) %>%
+  count() %>%
+  ungroup()%>%
+  arrange(desc(n))%>%
+  pivot_wider(id_cols = owner_gender, names_from = race_ethnicity, values_from = n)
+local_agregate
+
+ 
+######
+# do not forget to close the connection when you are finished
 dbDisconnect(con)
